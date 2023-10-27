0ca433637daba6f0dcde722796643b136a80927b
AllRoundJonU <42779702+AllRoundJonU@users.noreply.github.com>
2023-10-27 06:39:09 +0200

    Add parts of a new heist

---
+++ client/graverobbery.lua
@@ -0,0 +1,107 @@
+-- [[ Blip Variables ]] --
+CementaryLocation = {}
+local blipSpawned = false
+
+-- [[ Grave Variables ]] --
+local isDigging = false
+
+-- [[ Resource Metadata ]] -- 
+AddEventHandler('onResourceStop', function(resourceName)
+    if GetCurrentResourceName() == resourceName then
+        removeBlip()
+    end
+end)
+
+-- [[ Function for Grave Stones ]] --
+local ZoneSpawned = false 
+local ZoneCreated = {}
+
+CreateThread(function()
+    for k, v in pairs(Config.Graves) do
+        if ZoneSpawned then
+            return
+        end
+
+        for k, v in pairs(Config.Graves) do
+            if not ZoneCreated[k] then
+                ZoneCreated[k] = {}
+            end
+
+            ZoneCreated[k] = exports['qb-target']:AddBoxZone(v["GraveName"], v["Coords"], v["Length"], v["Width"], {
+                name = v["GraveName"],
+                heading = v["Heading"],
+                debugPoly = Config.DebugPoly,
+            }, {
+                options = {
+                    {
+                        icon = "fa-solid fa-trowel",
+                        label = "Dig Grave",
+                        event = "LENT-Graverobbery:Client:StartDigging",
+                    },
+                },
+        
+                distance = 1
+            })
+
+            ZoneSpawned = true
+        end
+    end
+end)
+
+-- [[ Events ]] -- 
+RegisterNetEvent('LENT-Graverobbery:Client:ResetGrave', function(OldGrave, state)
+    Config.Graves[OldGrave].Looted = state
+end)
+
+RegisterNetEvent("LENT-Graverobbery:Client:StartDigging", function()
+    if isDigging == false and QBCore.Functions.HasItem('shovel') then
+        local ped = PlayerPedId()
+        local playerPos = GetEntityCoords(ped)
+        TriggerEvent('animations:client:EmoteCommandStart', {"dig"})
+        for k, v in pairs(Config.Graves) do
+            local dist = #(GetEntityCoords(ped) - vector3(Config.Graves[k]["Coords"].x, Config.Graves[k]["Coords"].y, Config.Graves[k]["Coords"].z))
+            if dist <= 2 then
+                if Config.Graves[k].Looted == false then
+                    Config.Graves[k].Looted = true
+                    CurGrave = k
+                    QBCore.Functions.Progressbar("digging", "Digging...", math.random(8000, 15000), false, true, {
+                        disableMovement = true,
+                        disableCarMovement = false,
+                        disableMouse = false,
+                        disableCombat = true,
+                    }, {}, {}, {}, function() -- Done
+                        Diggin = true
+                        TriggerEvent('animations:client:EmoteCommandStart', {"c"})
+                        TriggerServerEvent('LENT-Graverobbery:Server:SetGraveState', CurGrave)
+                        TriggerServerEvent('LENT-Graverobbery:Server:GiveItems', CurGrave)
+                        policeAlert()
+                    end, function() -- Cancel
+                        TriggerEvent('animations:client:EmoteCommandStart', {"c"})
+                        QBCore.Functions.Notify("Cancelled.", "error")
+                    end)
+                elseif Config.Graves[k].Looted == true then
+                    QBCore.Functions.Notify('Seems like someone beat you to it!', 'error', 5000)
+                end
+            end
+        end
+    end
+end)
+
+-- [[ Blip Function ]] --
+if Config.Blip then
+    createBlip(vector3(-1683.29, -293.2, 51.89), 'Cemetery', 310, 39, 1.0, 3, false)
+end
+
+function removeBlip()
+    for i, GravBlip in pairs(CementaryLocation) do
+        removeBlip(GravBlip)
+    end
+end
+
+-- [[ Animation Functions ]] --
+function LoadAnimDict( dict )
+    while (not HasAnimDictLoaded(dict)) do
+        RequestAnimDict(dict)
+        Wait(5)
+    end
+end
---
+++ shared/heists/graverobbery_config.lua
@@ -0,0 +1,112 @@
+-- [[ Yard Blip ]] --
+Config.GraveBlip = true
+
+-- [[ Grave Reset Timer ]] --
+Config.GraveTimer = 300000 -- Default: 300000 = 5 Minutes
+
+-- [[ Rewards ]] --
+Config.GraveSpecialChance = 75
+Config.GraveChanceItem = "weapon_pistol"
+Config.GraveRewardChance = 20
+Config.GraveRewards ={
+    [1] = {
+        ["item"] = "rolex",
+        ["amount"] = {
+            ["min"] = 1,
+            ["max"] = 4
+        },
+    },
+    [2] = {
+        ["item"] = "diamond_ring",
+        ["amount"] = {
+            ["min"] = 1,
+            ["max"] = 4
+        },
+    },
+    [3] = {
+        ["item"] = "goldchain",
+        ["amount"] = {
+            ["min"] = 1,
+            ["max"] = 4
+        },
+    },
+    [4] = {
+        ["item"] = "10kgoldchain",
+        ["amount"] = {
+            ["min"] = 1,
+            ["max"] = 4
+        },
+    },
+    [5] = {
+        ["item"] = "goldbar",
+        ["amount"] = {
+            ["min"] = 1,
+            ["max"] = 4
+        },  
+    },
+    [6] = {
+        ["item"] = "grub",
+        ["amount"] = {
+            ["min"] = 1,
+            ["max"] = 4
+        },  
+    }, 
+}
+
+-- [[ Graves / Reset ]] --
+Config.Graves = {
+    [1] = {
+        ["GraveName"] = "Xirvin",
+        ["Coords"] = vector3(-1752.37, -205.8, 56.79),
+        ["Length"] = 1.6, ["Width"] = 0.6, ["Heading"] = 53,
+        ["Looted"] = false, -- Don't change this.
+    },
+    [2] = {
+        ["GraveName"] = "Serena",
+        ["Coords"] = vector3(-1729.41, -286.5, 49.83),
+        ["Length"] = 2.6, ["Width"] = 1.2, ["Heading"] = 10,
+        ["Looted"] = false, -- Don't change this.
+    },
+    [3] = {
+        ["GraveName"] = "Scarlett",
+        ["Coords"] = vector3(-1730.1, -282.35, 50.01),
+        ["Length"] = 2.2, ["Width"] = 1.0, ["Heading"] = 15,
+        ["Looted"] = false, -- Don't change this.
+    },
+    [4] = {
+        ["GraveName"] = "Jennifer",
+        ["Coords"] = vector3(-1745.68, -210.81, 56.81),
+        ["Length"] = 0.8, ["Width"] = 0.6, ["Heading"] = 55,
+        ["Looted"] = false, -- Don't change this.
+    },
+    [5] = {
+        ["GraveName"] = "Rick",
+        ["Coords"] = vector3(-1754.84, -218.82, 55.06),
+        ["Length"] = 1.0, ["Width"] = 0.6, ["Heading"] = 55,
+        ["Looted"] = false, -- Don't change this.
+    },
+    [6] = {
+        ["GraveName"] = "Sandra",
+        ["Coords"] = vector3(-1761.93, -204.7, 55.81),
+        ["Length"] = 1.0, ["Width"] = 0.6, ["Heading"] = 55,
+        ["Looted"] = false, -- Don't change this.
+    },
+    [7] = {
+        ["GraveName"] = "Peter",
+        ["Coords"] = vector3(-1752.89, -219.91, 55.08),
+        ["Length"] = 1.0, ["Width"] = 0.6, ["Heading"] = 55,
+        ["Looted"] = false, -- Don't change this.
+    },
+    [8] = {
+        ["GraveName"] = "Monica",
+        ["Coords"] = vector3(-1752.42, -213.23, 56.1),
+        ["Length"] = 1.0, ["Width"] = 0.6, ["Heading"] = 55,
+        ["Looted"] = false, -- Don't change this.
+    },
+    [9] = {
+        ["GraveName"] = "Ted",
+        ["Coords"] = vector3(-1746.37, -216.41, 55.93),
+        ["Length"] = 1.0, ["Width"] = 0.6, ["Heading"] = 55,
+        ["Looted"] = false, -- Don't change this.
+    },
+}
