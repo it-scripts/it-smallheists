ef0f7c1e068bfd7992ae8746b922f1c7d9f7e51b
Jon <unseldjonas0@gmail.com>
2023-10-27 03:00:56 +0200

    Local Changes
    Change the local system so it is easy to add new languages

---
+++ locales/de.lua
@@ -0,0 +1,60 @@
+Locales['de'] = {--
+    ['UNIVERSAL_NOTIFICATION_NO_POLICE'] = "Derzeit sind nicht genügend Polizeibeamte im Dienst",
+    ['UNIVERSAL_NOTIFICATION_ACTIVE_JOB'] = "Du bist bereits auf einer Mission, beende diese zuerst",
+    ['UNIVERSAL_NOTIFICATION_LOCATION'] = "Du wirst in Kürze eine E-Mail mit dem Standort erhalten.", 
+    ['UNIVERSAL_NOTIFICATION_COOLDOWN'] = "Sorry, aber ich habe aktuell keine Mission für dich. Schau später nochmal vorbei",
+    ['UNIVERSAL_NOTIFICATION_ACTIVE_HEIST'] = "Sorry, aber ich hab bereits jemanden für diesen Job",
+    ['UNIVERSAL_NOTIFICATION_CANCELED'] = "Abgebrochen...",
+    ['UNIVERSAL_NOTIFICATION_NO_TIME'] = "Du hast zu lange gebraucht, ich suche mit jemand anderen für die Mission",
+    ['UNIVERSAL_NOTIFICATION_NO_ITEM'] = "Du braucht ein %s um das zu machen", -- %s = item name
+
+    ['LAB_HEIST_MAIL_SENDER'] = "Lugo Bervic",
+    ['LAB_HEIST_MAIL_SUBJECT'] = "Bioforschung...",
+    ['LAB_HEIST_MAIL_MESSAGE_START'] = "Hier ist der Standort. Du musst die Firewall über den Computer in Labor 1 hacken und dann die Forschungsergebnisse herunterladen.<br/> Ich werde erneut eine E-Mail senden, wenn ich sehe, dass die Firewall nicht mehr funktioniert!",
+    ['LAB_HEIST_MAIL_MESSAGE_HACK'] = "Toll, dass du es geschafft hast! Gehe jetzt in den Kühlraum und bringe mir einige Arbeitsproben und alle Dateien, die du findest!",
+    ['LAB_HEIST_MAIL_MESSAGE_FINISH'] = "Bringe jetzt die Forschung, Proben und Dateien zurück zu mir für die bezahlung!",
+
+    ['LAB_HEIST_NOTIFICATION_GUARDS'] = "Wachen alarmiert!",
+    ['LAB_HEIST_NOTIFICATION_HACK_FAILED'] = "Du bist beim Hacken gescheitert, versuche es erneut",
+    ['LAB_HEIST_NOTIFICATION_NO_HACKING_DEVICE'] = "Du hast kein Hacking-Gerät",
+    ['LAB_HEIST_NOTIFICATION_POLICE_ALERT'] = "Einbruch in Humane Labs, Labor 1!",
+    ['LAB_HEIST_NOTIFICATION_DISABLED_ALARMS'] = "Du hast das Alarmsystem erfolgreich ausgeschaltet, geh rein",
+    ['LAB_HEIST_NOTIFICATION_FAIL_ALARMS'] = "Du konntest die Alarmanlage nicht deaktivieren, die Wachen wurden alarmiert",
+    ['LAB_HEIST_NOTIFICATION_SUCCESS_LOOT'] = "Du hast die Anlage erfolgreich geöffnet",
+    ['LAB_HEIST_NOTIFICATION_FAIL_LOOT'] = "Du konntest die Anlage nicht öffnen und ",
+    ['LAB_HEIST_NOTIFICATION_NO_LOOT_DEVICE'] = "Du hast kein Beutegerät",
+
+    ['LAB_HEIST_PROGRESSBAR_PICKUP'] = "Getting Job...",
+    ['LAB_HEIST_PROGRESSBAR_FIREWALL'] = "Firewall umgehen",
+    ['LAB_HEIST_PROGRESSBAR_DOWNLOAD'] = "Forschung herunterladen",
+    ['LAB_HEIST_PROGRESSBAR_FILES'] = "Beispiele und Dateien abrufen",
+    ['LAB_HEIST_PROGRESSBAR_SECURITY'] = "Sicherheitsalarme umgehen...",
+    ['LAB_HEIST_PROGRESSBAR_REROUTING'] = "Alarmüberprüfungen umleiten...",
+
+    ['LAB_HEIST_BLIP_LAB'] = "Bio Research Lab",
+    ['LAB_HEIST_BLIP_RESEARCH'] = "Research",
+    ['LAB_HEIST_BLIP_SECURITY'] = "Security bypass",
+
+    ['LAB_HEIST_TARGET_START_RAID_LAB'] = "Laborüberfall starten",
+    ['LAB_HEIST_TARGET_GET_PAYMENT'] = "Übergabe Forschung",
+    ['LAB_HEIST_TARGET_HACK_RESEARCH'] = "Forschungsdateien hacken",
+    ['LAB_HEIST_TARGET_SAMPLES'] = "Forschungsproben stehlen",
+    ['LAB_HEIST_TARGET_SECURITY'] = "Alarm umgehen (1 Versuch)",
+    ['LAB_HEIST_TARGET_LOCKPICK'] = "Lockpick",
+
+    ['CONTAINER_HEIST_MAIL_SENDER'] = "Jeremy C. Bickley",
+    ['CONTAINER_HEIST_MAIL_SUBJECT'] = "Container..",
+    ['CONTAINER_HEIST_MAIL_MESSAGE_LOCATION'] = "Hey, ich habe von ein paar Containern an den Docks erfahren, in denen sich wertvolle Sachen befinden sollen. <br/> Hier sind die Lagerpositionen <br/>",
+
+    ['CONTAINER_HEIST_NOTIFICATION_POLICE_ALERT'] = "Du hast nicht genug Geld",
+    ['CONTAINER_HEIST_NOTIFICATION_NO_MONEY'] = "Du hast den benötigten Gegenstand nicht",
+    ['CONTAINER_HEIST_NOTIFICATION_ITEM_BREAK'] = "Dein Dietrich ist kaputt",
+    ['CONTAINER_HEIST_NOTIFICATION_FINISHED'] = "Du hast den Raub beendet",
+    ['CONTAINER_HEIST_NOTIFICATION_FAIL_LOOT'] = "Du konntest den Container nicht öffnen, versuche es erneut",
+
+    ['CONTAINER_HEIST_PROGRESSBAR_PICKUP'] = "Job bekommen...",
+    ['CONTAINER_HEIST_PROGRESSBAR_LOOT'] = "Container plündern",
+
+    ['CONTAINER_HEIST_TARGET_START_HEIST'] = "Tip bekommen (%s$)", -- %s = amount
+    ['CONTAINER_HEIST_TARGET_CONTAINER'] = "Container öffnen",
+}
